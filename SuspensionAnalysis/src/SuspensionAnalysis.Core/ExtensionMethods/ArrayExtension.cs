4eb33b79d2edd977c8eac24bd9623378bf8f7450
MarVinMMN <77857390+MarVinMMN@users.noreply.github.com>
2021-01-22 20:24:11 -0300

    Added method CalculateCrossProduct and summary

--- SuspensionAnalysis/src/SuspensionAnalysis.Core/ExtensionMethods/ArrayExtension.cs
+++ SuspensionAnalysis/src/SuspensionAnalysis.Core/ExtensionMethods/ArrayExtension.cs
@@ -3,8 +3,16 @@ using System.Linq;
 
 namespace SuspensionAnalysis.Core.ExtensionMethods
 {
+    /// <summary>
+    /// It contains extension methods to arrays.
+    /// </summary>
     public static class ArrayExtension
     {
+        /// <summary>
+        /// This method normalizes tbe vector. 
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <returns></returns>
         public static double[] NormalizeVector(this double[] vector)
         {
             double vectorNorm = vector.CalculateNorm();
@@ -14,12 +22,37 @@ namespace SuspensionAnalysis.Core.ExtensionMethods
                 .ToArray();
         }
 
+        /// <summary>
+        /// This method calculates the vector norm. 
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <returns></returns>
         public static double CalculateNorm(this double[] vector)
         {
             double result = vector.Sum(v => Math.Pow(v, 2));
 
             return Math.Sqrt(result);
         }
+
+        /// <summary>
+        /// This method calculates the cross product. 
+        /// </summary>
+        /// <param name="vector1"></param>
+        /// <param name="vector2"></param>
+        /// <returns></returns>
         public static double[] CalculateCrossProduct(this double[] vector1, double[] vector2)
+        {
+            if (vector1.Length != 3 || vector2.Length != 3)
+            {
+                throw new ArgumentOutOfRangeException(nameof(CalculateCrossProduct), "The vector should have lenght equals to 3");
+            }
+
+            return new double[3]
+            {
+                vector1[1] * vector2[2] - vector1[2] * vector2[1],
+                vector1[2] * vector2[0] - vector1[0] * vector2[2],
+                vector1[0] * vector2[1] - vector1[1] * vector2[0],
+            };
+        }
     }
 }
