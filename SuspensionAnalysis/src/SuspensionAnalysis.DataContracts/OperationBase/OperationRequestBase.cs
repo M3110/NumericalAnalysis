6626cf8b2dec6f4495fc10818aacc51ef5faf469
M3110 <57352425+M3110@users.noreply.github.com>
2021-01-22 17:28:27 -0300

    Added Operation Base operation and data contracts.

---
+++ SuspensionAnalysis/src/SuspensionAnalysis.DataContracts/OperationBase/OperationRequestBase.cs
@@ -0,0 +1,19 @@
+﻿namespace SuspensionAnalysis.DataContracts.OperationBase
+{
+    /// <summary>
+    /// It represents the request content to operations.
+    /// </summary>
+    public class OperationRequestBase { }
+
+    /// <summary>
+    /// It represents the essencial request content to operations.
+    /// </summary>
+    /// <typeparam name="T">The type of request data.</typeparam>
+    public class OperationRequestBase<T> : OperationRequestBase
+    {
+        /// <summary>
+        /// Request data.
+        /// </summary>
+        public T Data { get; set; }
+    }
+}
