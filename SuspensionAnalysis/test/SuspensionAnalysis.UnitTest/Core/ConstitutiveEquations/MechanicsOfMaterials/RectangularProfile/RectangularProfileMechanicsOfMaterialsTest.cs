41fe7c831a7333d7fadf4768a2474f25a1fd9d2c
Emerson102414 <77986372+Emerson102414@users.noreply.github.com>
2021-02-22 17:50:24 -0300

    Added unit test to RectangularProfileMechanicsOfMaterials.

---
+++ SuspensionAnalysis/test/SuspensionAnalysis.UnitTest/Core/ConstitutiveEquations/MechanicsOfMaterials/RectangularProfile/RectangularProfileMechanicsOfMaterialsTest.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using SuspensionAnalysis.Core.ConstitutiveEquations.MechanicsOfMaterials.RectangularProfile;
+using SuspensionAnalysis.Core.GeometricProperty.RectangularProfile;
+
+namespace SuspensionAnalysis.UnitTest.Core.ConstitutiveEquations.MechanicsOfMaterials.RectangularProfile
+{
+    public class RectangularProfileMechanicsOfMaterialsTest
+    {
+        private readonly RectangularProfileMechanicsOfMaterials _operation;
+        private readonly Mock<IRectangularProfileGeometricProperty> _geometrypropertyMock;
+
+        public RectangularProfileMechanicsOfMaterialsTest()
+        {
+
+            this._geometrypropertyMock = new Mock<IRectangularProfileGeometricProperty>();
+            this._geometrypropertyMock.Setup()
+
+
+
+        }
+    }
+}
