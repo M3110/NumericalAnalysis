2f8c8021e80bddb32774a4b463195f2e3fed3d9d
MarVinMMN <77857390+MarVinMMN@users.noreply.github.com>
2021-04-01 18:23:55 -0300

    Created CalculateReactionsTest and added validation in suspension geometry.

---
+++ SuspensionAnalysis/test/SuspensionAnalysis.UnitTest/Core/Operations/CalculateReactions/CalculateReactionsTest.cs
@@ -0,0 +1,10 @@
+﻿using Operation = SuspensionAnalysis.Core.Operations.CalculateReactions;
+
+namespace SuspensionAnalysis.UnitTest.Core.Operations.CalculateReactions
+{
+    public class CalculateReactionsTest
+    {
+        private readonly Operation.CalculateReactions _operation;
+
+
+    }}
