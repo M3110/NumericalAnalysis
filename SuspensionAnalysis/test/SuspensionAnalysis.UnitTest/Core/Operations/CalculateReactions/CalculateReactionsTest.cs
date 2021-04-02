9da7b5b6d9a4c83b6e9ac4da9ec8ebc960f88d9d
Emerson102414 <77986372+Emerson102414@users.noreply.github.com>
2021-04-02 19:05:53 -0300

    Added part of CalculateReactionsTests constructor.

--- SuspensionAnalysis/test/SuspensionAnalysis.UnitTest/Core/Operations/CalculateReactions/CalculateReactionsTest.cs
+++ SuspensionAnalysis/test/SuspensionAnalysis.UnitTest/Core/Operations/CalculateReactions/CalculateReactionsTest.cs
@@ -1,5 +1,8 @@
 ﻿using Moq;
 using SuspensionAnalysis.Core.Mapper;
+using SuspensionAnalysis.Core.Models.SuspensionComponents;
+using SuspensionAnalysis.DataContracts.CalculateReactions;
+using SuspensionAnalysis.UnitTest.Helper;
 using Operation = SuspensionAnalysis.Core.Operations.CalculateReactions;
 
 namespace SuspensionAnalysis.UnitTest.Core.Operations.CalculateReactions
@@ -10,10 +13,19 @@ namespace SuspensionAnalysis.UnitTest.Core.Operations.CalculateReactions
 
         private readonly Mock<IMappingResolver> _mappingResolverMock;
 
+        private readonly CalculateReactionsRequest _requestStub;
+
+        private readonly SuspensionSystem _suspensionSystem;
+
         public CalculateReactionsTest()
         {
-            this._mappingResolverMock = new Mock<IMappingResolver>();
+            this._requestStub = CalculateReactionsHelper.CreateRequest();
 
+            this._mappingResolverMock = new Mock<IMappingResolver>();
+            this._mappingResolverMock
+                .Setup(mr => mr.MapFrom(this._requestStub))
+                .Returns(_suspensionSystem);
+                
             this._operation = new Operation.CalculateReactions(this._mappingResolverMock.Object);
         }
     }
