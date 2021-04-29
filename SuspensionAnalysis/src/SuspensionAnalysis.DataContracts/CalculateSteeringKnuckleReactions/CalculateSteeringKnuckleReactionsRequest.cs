9027903455dd5ace2c94ca015953023d7284df13
M3110 <57352425+M3110@users.noreply.github.com>
2021-04-29 18:56:46 -0300

    Fixes CalculateSteeringKnuckleReactions data contracts.

--- SuspensionAnalysis/src/SuspensionAnalysis.DataContracts/CalculateSteeringKnuckleReactions/CalculateSteeringKnuckleReactionsRequest.cs
+++ SuspensionAnalysis/src/SuspensionAnalysis.DataContracts/CalculateSteeringKnuckleReactions/CalculateSteeringKnuckleReactionsRequest.cs
@@ -1,13 +1,10 @@
 ﻿using SuspensionAnalysis.DataContracts.Models.SuspensionComponents;
 using SuspensionAnalysis.DataContracts.OperationBase;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SuspensionAnalysis.DataContracts.CalculateSteeringKnuckleReactions
 {
     /// <summary>
-    /// It represents the request content to  CalculateSteeringKnuckleReactions operation.
+    /// It represents the request content to CalculateSteeringKnuckleReactions operation.
     /// </summary>
     public class CalculateSteeringKnuckleReactionsRequest : OperationRequestBase
     {
